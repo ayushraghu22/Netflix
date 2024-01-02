be5e255833c10e0351524013dcc6ec060a24a849
Ayush <ayushraghuwanshi22@gmail.com>
2024-01-02 16:54:23 +0530

    Navbar done

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import "./Navbar.scss";
+import netflixLogo from "../../images/netflix.png";
+import { ArrowDropDown, Notifications, Search } from "@mui/icons-material";
+import { useState } from "react";
+
+function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.scrollY === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  console.log(isScrolled);
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img src={netflixLogo} alt="Netflix logo" />
+
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+
+        <div className="right">
+          <Search className="icons" />
+          <span>Kid</span>
+          <Notifications className="icons" />
+          <img
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"
+            alt="profile"
+          />
+
+          <div className="profile">
+            <ArrowDropDown className="icons" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,15 @@
+import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
-import React from "react";
 
 const Home = () => {
   return (
     <div className="home">
+        <Navbar />
+        <img
+            width="100%"
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"
+            alt="profile"
+          />
     </div>
   );
 };
